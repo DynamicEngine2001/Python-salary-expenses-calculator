69955581178c6cd27373703542ee2d2805f899c0
Olaf Gladis <olafgladis@gmail.com>
2020-10-04 09:27:41 +0200

    Add better Errorhandling
    Each input will be tried 3 times, if the conversion to float fails, it will be tried again.
    If it fails 3 times, the applications exits with an error code.

--- PR3.py
+++ PR3.py
@@ -1,8 +1,27 @@
+import sys
+
 print("The salary and expenses Calculator:")
-salary = input ("Insert the value of your salary:")
-s = float(salary)
-expenses = input ("Insert the value of your expenses:")
-e=float(expenses)
+for _ in range(3):
+  salary = input ("Insert the value of your salary:")
+  try:
+    s = float(salary)
+    break
+  except Exception as e:
+    print("Please enter a valid Number")
+else:
+  print("couldn't read a valid salary number")
+  sys.exit(1)
+
+for _ in range(3):
+  expenses = input ("Insert the value of your expenses:")
+  try:
+    e=float(expenses)
+    break
+  except Exception as e:
+    print("Please enter a valid Number")
+else:
+  print("couldn't read a valid expense number")
+  sys.exit(1)
+
 percentage = (e*100)/s
 print("Your spend ",percentage ,"%", "of your salary in your expenses.")
-print("Enter A valid Number !")
